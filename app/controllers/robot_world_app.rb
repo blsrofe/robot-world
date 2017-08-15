269f66305e5c43fd87dbb91d7918c9c3fb643477
Becki Srofe <blf_23@yahoo.com>
2017-08-15 16:58:17 -0600

    Add database table

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -2,9 +2,10 @@ require_relative '../models/robot.rb'
 
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
   get '/robots' do
+    @robots = Robot.all
     erb :index
   end
 end
-
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,8 +1,25 @@
 require 'sqlite3'
 
+attr_reader :name,
+            :city,
+            :state,
+            :department
+
 class Robot
   def initialize(robot_params)
+    @name = robot_params["name"]
+    @city = robot_params["city"]
+    @state = robot_params["state"]
+    @department = robot_params["department"]
     @database = SQLite3::Database.new('db/robot_world_development.db')
     @database.results_as_hash = true
+  end
+
+  def self.all
+    database
+    robots = database.execute("SELECT * FROM robots")
+    robots.map do |robot|
+      Robot.new(robot)
+    end
   end
 end
